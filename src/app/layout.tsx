9d1e4f03050c0cabdb929a5e0e1168ff6c1c46db
Nastechy <Ibinichinasajoy@gmail.com>
2025-08-03 22:29:40 +0100

    modify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,13 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: 'Aso Realty',
   description: 'Find your dream home or investment with Aso Realty.',
-  viewport: 'width=device-width, initial-scale=1',
 };
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+};
+
 
 export default function RootLayout({
   children,
